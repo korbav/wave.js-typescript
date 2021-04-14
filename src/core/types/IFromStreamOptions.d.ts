91c0bf0d6f79fecd36744b022b60fe22c1dd7570
Bilel OURAL <bilel.oural@yahoo.fr>
2021-04-14 12:21:11 +0000

    fixing safari problem with streams and user interaction

--- src/core/types/IFromStreamOptions.d.ts
+++ src/core/types/IFromStreamOptions.d.ts
@@ -1,3 +1,4 @@
 export default interface IFromStreamOptions {
     connectDestination?: boolean;
+    skipUserEventsWatcher?: boolean;
 }
--- src/handlers/from-stream.ts
+++ src/handlers/from-stream.ts
@@ -13,6 +13,15 @@ const renderFrame = (currentStream, analyser, sources, stream, frameCount) => {
   }
 }
 
+const events = ['touchstart', 'touchmove', 'touchend', 'mouseup', 'click'];
+
+let interacted = false;
+const registerProcessAfterUserEvent = (process) => {
+  events.forEach((event) => {
+    document.body.addEventListener(event, () => { interacted = true; process(); }, {once: true})
+  });
+}
+
 export default function fromStream(
   stream: MediaStream,
   canvasId: string,
@@ -26,45 +35,64 @@ export default function fromStream(
 
   const parsedFromStreamOptions = {
     connectDestination: true,
+    skipUserEventsWatcher: false,
     ...(fromStreamOptions || {}),
   };
 
   checkGenerator(parsedOptions.type);
 
-  const {connectDestination} = parsedFromStreamOptions;
+  const {connectDestination, skipUserEventsWatcher} = parsedFromStreamOptions;
   const currentStream = {canvasId, options: parsedOptions, data: null, loop: null, animation: null, activated: true};
   const sources = initGlobalObject('stream-sources', parsedOptions.globalAccessKey);
 
-  let audioCtx, analyser, source;
-  if (!sources[stream.toString()]) {
-    audioCtx = new AudioContext();
-    analyser = audioCtx.createAnalyser();
-    source = audioCtx.createMediaStreamSource(stream);
-    source.connect(analyser);
-    if (connectDestination) {
-      source.connect(audioCtx.destination); //playback audio
-    }
-    sources[stream.toString()] = {
-      audioCtx,
-      analyser,
-      source
-    }
-  } else {
-    cancelAnimationFrame(sources[stream.toString()].animation);
-    analyser = sources[stream.toString()].analyser;
-  }
+  let audioCtx;
 
-  analyser.fftsize = 32768;
-  const bufferLength = analyser.frequencyBinCount;
-  currentStream.data = new Uint8Array(bufferLength);
-  this.frameCount = 1;
-  currentStream.loop = () => renderFrame(currentStream, analyser, sources, stream, ++this.frameCount);
-  renderFrame(currentStream, analyser, sources, stream, 1);
+  const process = () => {
+    let analyser, source;
+    if (!sources[stream.toString()]) {
+      audioCtx = new AudioContext();
+      analyser = audioCtx.createAnalyser();
+
+      // Test Safari
+      analyser.minDecibels = -90;
+      analyser.maxDecibels = -10;
+      analyser.smoothingTimeConstant = 0.85;
+      // End test safari
+
+
+      source = audioCtx.createMediaStreamSource(stream);
+      source.connect(analyser);
+      if (connectDestination) {
+        analyser.connect(source.destination);
+      }
+      sources[stream.toString()] = {
+        audioCtx,
+        analyser,
+        source
+      }
+    } else {
+      cancelAnimationFrame(sources[stream.toString()].animation);
+      analyser = sources[stream.toString()].analyser;
+    }
+
+    analyser.fftsize = 32768;
+    const bufferLength = analyser.frequencyBinCount;
+    currentStream.data = new Uint8Array(bufferLength);
+    this.frameCount = 1;
+    currentStream.loop = () => renderFrame(currentStream, analyser, sources, stream, ++this.frameCount);
+    renderFrame(currentStream, analyser, sources, stream, 1);
+  };
+
+  if (interacted || !skipUserEventsWatcher) {
+    registerProcessAfterUserEvent(() => process());
+  } else {
+    process();
+  }
 
   return {
     deactivate: () => {
       clearCanvas(document.getElementById(currentStream.canvasId) as HTMLCanvasElement);
-      audioCtx.close().then();
+      audioCtx && audioCtx.close().then();
       currentStream.activated = false;
     }
   };
